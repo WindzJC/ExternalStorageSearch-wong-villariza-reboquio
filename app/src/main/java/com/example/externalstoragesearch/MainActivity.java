9689290d241976adc2f0e401241a3dfbd6ef2f80
WindzJC <windellejcr@gmail.com>
2023-02-28 19:30:39 +0800

    exstoragesearch

---
+++ app/src/main/java/com/example/externalstoragesearch/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.externalstoragesearch;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    Button save, search;
+    EditText input;
+
+    private String filename = "SampleFile.txt";
+    private String filepath = "MyFileStorage";
+    File myExternalFile;
+    String myData = "";
+
+    @SuppressLint({"SetTextI18n", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        save = findViewById(R.id.save);
+        search = findViewById(R.id.search);
+        input = findViewById(R.id.input);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(myExternalFile);
+                    fos.write(input.getText().toString().getBytes());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                input.setText("");
+                Toast.makeText(getApplicationContext(), "String saved to external storage", Toast.LENGTH_SHORT).show();
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileInputStream fis = new FileInputStream(myExternalFile);
+                    DataInputStream in = new DataInputStream(fis);
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        myData = myData + strLine;
+                    }
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //Get text in input compare to text saved
+                String lowerCaseResult = myData.toLowerCase();
+                String searchInput = input.getText().toString().toLowerCase();
+                boolean search = lowerCaseResult.contains(searchInput);
+                //Displaying Toast with Hello Javatpoint message
+                if (search) {
+                    int index = lowerCaseResult.indexOf(searchInput);
+                    Toast.makeText(getApplicationContext(), "Word found at position " + index , Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Word not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            save.setEnabled(false);
+        } else {
+            myExternalFile = new File(getExternalFilesDir(filepath), filename);
+        }
+    }
+
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+}
